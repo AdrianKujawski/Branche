ea1cea3ac03339bcfc1f08c3b94752810c846723
Adrian <Adrian@DESKTOP-H1SR00I>
2016-12-03 10:10:18 +0100

    Dodanie klasy MyMath i StringOperation

--- Branche/Program.cs
+++ Branche/Program.cs
@@ -9,4 +9,24 @@ namespace Branche {
 		static void Main(string[] args) {
 		}
 	}
+
+
+	class MyMath {
+
+		static int Add(int firstNumber, int secondNumber) {
+			return firstNumber + secondNumber;
+		}
+
+		static int Sub(int firstNumber, int secondNumber) {
+			return firstNumber - secondNumber;
+		}
+	}
+
+
+	class StringOperation {
+
+		static string JoinTexts(string firstText, string secondString) {
+			return firstText + secondString;
+		}
+	}
 }
